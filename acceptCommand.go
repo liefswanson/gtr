13f3be6e52d7c22d10088d96f5add2541df021c7
liefswanson <liefs@sfu.ca>
2016-10-14 18:29:49 -0700

    fix bug in accepting single tests, and change accept -all for my workflow

--- acceptCommand.go
+++ acceptCommand.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"os/exec"
 
 	"github.com/fatih/color"
 )
@@ -38,63 +39,75 @@ func acceptCommand(flags acceptFlags, testname string) {
 
 func acceptAll() {
 	os.RemoveAll(backupDir)
-	os.Rename(expectDir, backupDir)
-	os.Rename(resultDir, expectDir)
-	initResultDirs()
+	// os.Rename(expectDir, backupDir)
+	// os.Rename(resultDir, expectDir)
+	// initResultDirs()
+	backup()
+	acceptAllPika()
+	acceptAllStandalone()
+}
+
+func acceptAllPika() {
+	files := getAllFiles(pikaDir)
+	files = filterOutFiles(files, ".gitignore")
+
+	for _, file := range files {
+		testname := replaceExtension(file.Name(), "")
+		acceptCodeGen(testname)
+		acceptOptimized(testname)
+		acceptCompile(testname)
+	}
+}
+
+func acceptAllStandalone() {
+	files := getAllFiles(asmDir)
+	files = filterOutFiles(files, ".gitignore")
+
+	for _, file := range files {
+		testname := replaceExtension(file.Name(), "")
+		acceptOptimizedStandalone(testname)
+	}
+}
+
+func backup() {
+	exec.Command("cp", "-rf", expectDir, backupDir).Run()
+}
+
+func acceptTest(testname, oldPath, newPath, ext string) {
+	old := buildPath(oldPath, testname+ext)
+	new := buildPath(newPath, testname+ext)
+	moveIfExists(old, new)
 }
 
 func acceptCompile(testname string) {
-	new := buildPath(result.build.compiler, testname+txtExt)
-	old := buildPath(expect.build.compiler, testname+txtExt)
-	moveIfExists(old, new)
+	acceptTest(testname, result.build.compiler, expect.build.compiler, txtExt)
+	acceptTest(testname, result.run.compiler, expect.run.compiler, txtExt)
+	acceptTest(testname, result.asm.compiler, expect.asm.compiler, asmExt)
 
-	new = buildPath(result.asm.compiler, testname+asmExt)
-	old = buildPath(expect.asm.compiler, testname+asmExt)
-	moveIfExists(old, new)
-
-	new = buildPath(result.run.compiler, testname+txtExt)
-	old = buildPath(expect.run.compiler, testname+txtExt)
-	moveIfExists(old, new)
+	acceptTest(testname, result.buildo.compiler, expect.buildo.compiler, txtExt)
+	acceptTest(testname, result.asmo.compiler, expect.asmo.compiler, asmoExt)
 }
 
 func acceptCodeGen(testname string) {
-	new := buildPath(result.build.codegenerator, testname+txtExt)
-	old := buildPath(expect.build.codegenerator, testname+txtExt)
-	moveIfExists(old, new)
-
-	new = buildPath(result.asm.codegenerator, testname+asmExt)
-	old = buildPath(expect.asm.codegenerator, testname+asmExt)
-	moveIfExists(old, new)
-
-	new = buildPath(result.run.codegenerator, testname+txtExt)
-	old = buildPath(expect.run.codegenerator, testname+txtExt)
-	moveIfExists(old, new)
+	acceptTest(testname, result.build.codegenerator, expect.build.codegenerator, txtExt)
+	acceptTest(testname, result.run.codegenerator, expect.run.codegenerator, txtExt)
+	acceptTest(testname, result.asm.codegenerator, expect.asm.codegenerator, asmExt)
 }
 
 func acceptOptimized(testname string) {
-	new := buildPath(result.build.optimizer, testname+txtExt)
-	old := buildPath(expect.build.optimizer, testname+txtExt)
-	moveIfExists(old, new)
+	acceptTest(testname, result.build.optimizer, expect.build.optimizer, txtExt)
+	acceptTest(testname, result.run.optimizer, expect.run.optimizer, txtExt)
+	acceptTest(testname, result.asm.optimizer, expect.asm.optimizer, asmoExt)
 
-	new = buildPath(result.asm.optimizer, testname+asmoExt)
-	old = buildPath(expect.asm.optimizer, testname+asmoExt)
-	moveIfExists(old, new)
-
-	new = buildPath(result.run.optimizer, testname+txtExt)
-	old = buildPath(expect.run.optimizer, testname+txtExt)
-	moveIfExists(old, new)
+	acceptTest(testname, result.buildo.optimizer, expect.buildo.optimizer, txtExt)
+	acceptTest(testname, result.asmo.optimizer, expect.asmo.optimizer, asmoExt)
 }
 
 func acceptOptimizedStandalone(testname string) {
-	new := buildPath(result.build.optimizerStandalone, testname+txtExt)
-	old := buildPath(expect.build.optimizerStandalone, testname+txtExt)
-	moveIfExists(old, new)
+	acceptTest(testname, result.build.optimizerStandalone, expect.build.optimizerStandalone, txtExt)
+	acceptTest(testname, result.run.optimizerStandalone, expect.run.optimizerStandalone, txtExt)
+	acceptTest(testname, result.asm.optimizerStandalone, expect.asm.optimizerStandalone, asmoExt)
 
-	new = buildPath(result.asm.optimizerStandalone, testname+asmoExt)
-	old = buildPath(expect.asm.optimizerStandalone, testname+asmoExt)
-	moveIfExists(old, new)
-
-	new = buildPath(result.run.optimizerStandalone, testname+txtExt)
-	old = buildPath(expect.run.optimizerStandalone, testname+txtExt)
-	moveIfExists(old, new)
+	acceptTest(testname, result.buildo.optimizerStandalone, expect.buildo.optimizerStandalone, txtExt)
+	acceptTest(testname, result.asmo.optimizerStandalone, expect.asmo.optimizerStandalone, asmoExt)
 }
